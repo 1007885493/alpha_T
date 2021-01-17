e2b081d210204d7472c7d28ba9dd3e79e53e6d0d
Administrator <1007885493@qq.com>
2021-01-17 18:44:25 +0800

    1

--- 爬虫组/爬虫整合.py
+++ 爬虫组/爬虫整合.py
@@ -1,10 +1,12 @@
 # 将之前验证可用的爬虫们进行整理与合并,提高便捷性
-
+import requests
 
 
 def spiderFactory(url, cookies):
     # requests链接
-    # 正则筛选
+    data_R = requests.get(url,cookies).text
+    # 正则筛选_根据不同需求
+
     # 数据打包
     dataPackage = object
     pass
