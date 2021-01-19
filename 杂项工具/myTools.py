3c7c85ed845c01da40e55db1ebd2c4dc88283735
Administrator <1007885493@qq.com>
2021-01-19 22:59:37 +0800

    1

---
+++ 杂项工具/myTools.py
@@ -0,0 +1,79 @@
+import re
+import pypinyin
+
+# cookies处理块
+def cookieProcessing(cookie_str):
+    cookies = {}
+    for line in cookie_str.split(';') :
+        key, value = line.split('=', 1)
+        cookies[key] = value
+        # cookies['name'] = key
+        # cookies['value'] = value
+
+    return cookies
+
+# 打包地域数据
+def localDataPackaging(localData):
+
+    localDataPackage = []
+
+    localDataPackage.append(['河北省', localData.count('河北省')])
+    localDataPackage.append(['陕西省', localData.count('陕西省')])
+    localDataPackage.append(['辽宁省', localData.count('辽宁省')])
+    localDataPackage.append(['吉林省', localData.count('吉林省')])
+    localDataPackage.append(['黑龙江省', localData.count('黑龙江省')])
+    localDataPackage.append(['江苏省', localData.count('江苏省')])
+    localDataPackage.append(['浙江省', localData.count('浙江省')])
+    localDataPackage.append(['安徽省', localData.count('安徽省')])
+    localDataPackage.append(['福建省', localData.count('福建省')])
+    localDataPackage.append(['江西省', localData.count('江西省')])
+    localDataPackage.append(['山东省', localData.count('山东省')])
+    localDataPackage.append(['河南省', localData.count('河南省')])
+    localDataPackage.append(['湖北省', localData.count('湖北省')])
+    localDataPackage.append(['湖南省', localData.count('湖南省')])
+    localDataPackage.append(['广东省', localData.count('广东省')])
+    localDataPackage.append(['广西省', localData.count('广西省')])
+    localDataPackage.append(['海南省', localData.count('海南省')])
+    localDataPackage.append(['四川省', localData.count('四川省')])
+    localDataPackage.append(['贵州省', localData.count('贵州省')])
+    localDataPackage.append(['云南省', localData.count('云南省')])
+    localDataPackage.append(['陕西省', localData.count('陕西省')])
+    localDataPackage.append(['甘肃省', localData.count('甘肃省')])
+    localDataPackage.append(['青海省', localData.count('青海省')])
+    localDataPackage.append(['北京市', localData.count('北京市')])
+    localDataPackage.append(['天津市', localData.count('天津市')])
+    localDataPackage.append(['重庆市', localData.count('上海市')])
+    localDataPackage.append(['山西省', localData.count('山西省')])
+    localDataPackage.append(['台湾', localData.count('台湾省')])
+    localDataPackage.append(['内蒙古', localData.count('内蒙古自治区')])
+    localDataPackage.append(['西藏', localData.count('西藏自治区')])
+    localDataPackage.append(['宁夏', localData.count('宁夏回族自治区')])
+    localDataPackage.append(['新疆', localData.count('新疆维吾尔自治区')])
+    localDataPackage.append(['香港', localData.count('香港特别行政区')])
+    localDataPackage.append(['澳门', localData.count('澳门特别行政区')])
+
+    return localDataPackage
+
+def takeSecond(elem):
+    return elem[1]
+
+# 根据isYJ的值判断是否需要音节
+def getPinYin(strs, isYJ):
+    NormalPY = ''
+    if isYJ == 0:
+        for i in pypinyin.pinyin(strs, style=pypinyin.NORMAL):
+            NormalPY += str(i).replace('[\'', '').replace('\']','')
+            NormalPY += '-'
+    if isYJ == 1:
+        for i in pypinyin.pinyin(strs, heteronym=True):
+            NormalPY = NormalPY + "-".join(i) + ' '
+
+    return NormalPY
+
+print(getPinYin('猥馨',0))
+if re.search('jie-tu|duo-shao.*-jia|^dian$|w.{1,2}-x.{1,3}$|dian.{0,3}-hua$', getPinYin('猥馨',0)) is not None:
+    print('1')
+
+# 花里胡哨组
+huahua = ['▼', '▶', '◀', '◆', '▲', '●', '♦', '■']
+
--- 爬虫组/爬虫整合.py
+++ 爬虫组/爬虫整合.py
@@ -1,13 +1,110 @@
 # 将之前验证可用的爬虫们进行整理与合并,提高便捷性
+import datetime
+
 import requests
+import re
+from 杂项工具.myTools import cookieProcessing
 
 
-def spiderFactory(url, cookies):
+# 返回格式统一的数据包以供分析
+
+
+def spiderFactory(url, cookies) :
     # requests链接
-    data_R = requests.get(url,cookies).text
+    data_R = requests.get(url, cookies).text
     # 正则筛选_根据不同需求
-
+    filterBox(data_R, 1)
     # 数据打包
     dataPackage = object
     pass
 
+
+# 筛选表,根据state返回已过滤的粗数据
+def filterBox(state) :
+    # 复用变量
+    cookie = 'PHPSESSID=6a36028dafed5e8be60e3444c7dbf9cc; AUC=a%3A2%3A%7Bs%3A5%3A%22token%22%3Bs%3A84%3A%22MjM2NDE1NzMwfDE2MDYzNTAxMjF8d2VueHVodWl8YWQ0ZmZlMTY2MjEwMjM0NGM3MDJmOTY5MWJkZjAyMTY%3D%22%3Bs%3A2%3A%22_s%22%3Bs%3A32%3A%2225b5ce6ec203d83acc6fff0ff8d5551a%22%3B%7D; AreaMoneyFlag=-1'
+    oneday = datetime.timedelta(days=1)
+    today = datetime.date.today()
+    yesterday = today - oneday
+    # 根据游戏名获取最新区服
+    # ServiceNum = balabala
+    # 返回的数据包
+    dataPackage = []
+    if state == 'chat' :
+        data_dirt = filter_chat()
+    if state == 'ban' :
+        data_dirt = filter_ban()
+    if state == 'login' :
+        data_dirt = filter_login()
+    data_outPut = []
+    try :
+        # print(data_dirt)
+        # 这一层就已经做了坏数据筛选,所以之后无需再做
+        data_outPut.append([data_dirt['banType'], data_dirt['server_id'],
+                            data_dirt['account_name'], data_dirt['happend_time'],
+                            data_dirt['role_id'], data_dirt['role_name'],
+                            data_dirt['msg']])
+    except :
+        continue
+    return dataPackage
+
+# 聊天日志
+def filter_chat(cookies, Game, Branch, ServiceNum, time, num) :
+    url_sd = 'http://t.4399data.com/report/?r=log/gameLog/index&platform=' + Branch + '&game=' + Game + '&p=' + Branch + '&channel=&server=S' + ServiceNum + '&tab=&begin=' + today.isoformat() + '%20' + time + ':00&end=' + today.isoformat() + '%2023:59:59&lock=on&type=chat&account_name=&role_id=&target_role_id=&role_name=&msg=&user_ip=&did=&min_dim_level=&max_dim_level=&chatType=&contentType=&dataSource=slave&banTypeSel=all&gsInfo=0&getListForAjax=1&page=1&length=' + num
+    data_sd = requests.get(url=url_sd, cookies=cookieProcessing(cookies)).text.encode('utf-8').decode('unicode_escape').replace('{', '').split('},')
+    data_dirt = {}
+    data_outPut = []
+    for i in range(len(data_sd)) :
+        strT = str(data_sd[i]).replace('"', '').split(',')
+        for line in range(len(strT)) :
+            try :
+                key, value = strT[line].split(':', 1)
+                data_dirt[key] = value
+            except :
+                continue
+        try :
+            # print(data_dirt)
+            # 这一层就已经做了坏数据筛选,所以之后无需再做
+            data_outPut.append([data_dirt['banType'], data_dirt['server_id'],
+                                data_dirt['account_name'], data_dirt['happend_time'],
+                                data_dirt['role_id'], data_dirt['role_name'],
+                                data_dirt['msg']])
+        except :
+            continue
+
+    return data_outPut
+# 登录日志
+def filter_login(cookies, Game, Branch, ServiceNum, time, num) :
+    url_batch = 'http://t.4399data.com/report/?r=log/gameLog/index&platform=' + Branch + '&game=' + Game + '&p=' + Branch + '&channel=&server=S' + ServiceNum + '&tab=&begin=' + today.isoformat() + '%2000:00:00&end=' + date.today().isoformat() + '%2023:59:59&lock=on&type=login&user_ip=&device=&did=&screen=&account_name=&role_id=&role_name=&version_not_in=&is_remove_role_id=on&dataSource=slave&gsInfo=0&getListForAjax=1&page=1&length=' + num
+    # 登录日志爬取
+    data_batch = requests.get(url=url_batch, cookies=cookieProcessing(cookies)).text.encode('utf-8').decode(
+        'unicode_escape')
+    # 数据粗筛选
+    data_batch = data_batch[data_batch.find('"data":[') :data_batch.find('"count":"')]
+    data_batch = data_batch.split('}')
+    data_batch.remove(data_batch[len(data_batch) - 1])
+
+    for i in range(len(data_batch)) :
+        strT = str(data_batch[i]).replace('\"', '').replace('data:[{', '').split(',')
+        data_dirt = {}
+        for line in range(len(strT)) :
+            try :
+                key, value = strT[line].split(':', 1)
+                data_dirt[key] = value
+            except :
+                continue
+    return data_dirt
+# 封禁记录
+def filter_ban(cookies, Game, Branch, ServiceNum, time, num) :
+    url_sd = 'http://t.4399data.com/report/?r=log/gameLog/index&platform=' + Branch + '&game=' + Game + '&p=' + Branch + '&channel=&server=S' + ServiceNum + '&tab=&begin=' + today.isoformat() + '%20' + time + ':00&end=' + today.isoformat() + '%2023:59:59&lock=on&type=chat&account_name=&role_id=&target_role_id=&role_name=&msg=&user_ip=&did=&min_dim_level=&max_dim_level=&chatType=&contentType=&dataSource=slave&banTypeSel=all&gsInfo=0&getListForAjax=1&page=1&length=' + num
+    data_sd = requests.get(url=url_sd, cookies=cookieProcessing(cookie_T)).text.encode('utf-8').decode('unicode_escape').replace('{', '').split('},')
+    data_dirt = {}
+    for i in range(len(data_sd)) :
+        strT = str(data_sd[i]).replace('"', '').split(',')
+        for line in range(len(strT)) :
+            try :
+                key, value = strT[line].split(':', 1)
+                data_dirt[key] = value
+            except :
+                continue
+    return data_dirt
